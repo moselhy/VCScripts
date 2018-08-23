3a17633626b94cb3c3469db8fbdfce78cab89f28
Mohamed Moselhy <mohamed.moselhy@uwo.ca>
2018-08-23 15:19:19 +0200

    If on GenOuest server, load modules automatically

--- rungatk.bash
+++ rungatk.bash
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 # USAGE: rungatk.bash chromNumber
+# Must have the following files in the working directory: CHR.fasta CHR_PE1.fastq CHR_PE2.fastq; where `CHR` is the chromosome number
 
 # YOU MUST MODIFY THE FOLLOWING VARIABLES TO POINT TO THE CORRECT BINARIES
 BWA="/local/bwa/bwa-0.7.10/bwa"
@@ -8,6 +9,14 @@ PICARD="/local/miniconda3/envs/picard-2.18.2/bin/picard"
 SAMTOOLS="/local/miniconda3/envs/samtools-1.6/bin/samtools"
 GATK="$HOME/bin/gatk"
 
+# If on the GenOuest cluster, you don't need to modify the first 3 values above because the following block will load them
+# (you will still need to download GATK because it is outdated on GO)...
+if [ $(hostname -f | cut -d . -f2,3) == "genouest.org" ]; then
+	source /local/env/envjava-1.8.0.sh
+	source /local/env/envbwa-0.7.10.sh
+	source /local/env/envpicard-2.18.2.sh
+	source /local/env/envsamtools-1.6.sh
+fi
 
 ### START PIPELINE ###
 
